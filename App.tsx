8d64e48364a6910cc65b0ef62d710f3d75e96865
rohit <rohit.880207@gmail.com>
2025-05-10 14:49:54 +0530

    added menuProvider and safeAreaView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/home';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MenuProvider } from 'react-native-popup-menu';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MenuProvider>
+    <SafeAreaProvider style={{ flex: 1, backgroundColor: '#faf9f4' }}>
+    <SafeAreaView style={styles.container}>
+  <Home/>
+  </SafeAreaView>
+  </SafeAreaProvider>
+  </MenuProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#faf9f4',
   },
 });
